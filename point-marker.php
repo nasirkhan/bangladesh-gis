537f90c4f266f4e5921274c2748943d9e958c987
nasirkhan <nasir8891@gmail.com>
2013-06-06 15:36:47 +0600

    first dynamic layer | UHC

--- point-marker.php
+++ point-marker.php
@@ -1,6 +1,13 @@
 <?php
 require_once 'configuration.php';
 
+/**
+ * Get all the UHC
+ */
+$sql = "SELECT * FROM organisationunit WHERE featureType LIKE 'point' AND type LIKE 'Upazila Health Complex'";
+$result = mysql_query($sql)or die(mysql_error()."<br>Query<br>_____<br>$sql<br>");
+$row_count = mysql_num_rows($result);
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -48,19 +55,19 @@ require_once 'configuration.php';
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
         <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
         <link rel="shortcut icon" href="../assets/ico/favicon.png">
-        
+
         <!-- Leafletjs CDN css link -->
-	<!-- <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5/leaflet.css" /> -->
-	<link rel="stylesheet" href="library/leafletjs/leaflet.css" />
-	<!-- Font Awesome local -->
-	<link rel="stylesheet" href="library/font-awesome/css/font-awesome.min.css">
-	<!-- <link href="http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" rel="stylesheet"> -->
-	<link rel="stylesheet" href="library/leaflet.awesome-markers/leaflet.awesome-markers.css">
-	
-	
- 	<!--[if lte IE 8]>
-    	<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5/leaflet.ie.css" />
-    	<![endif]-->
+        <!-- <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5/leaflet.css" /> -->
+        <link rel="stylesheet" href="library/leafletjs/leaflet.css" />
+        <!-- Font Awesome local -->
+        <link rel="stylesheet" href="library/font-awesome/css/font-awesome.min.css">
+        <!-- <link href="http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" rel="stylesheet"> -->
+        <link rel="stylesheet" href="library/leaflet.awesome-markers/leaflet.awesome-markers.css">
+
+
+        <!--[if lte IE 8]>
+        <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5/leaflet.ie.css" />
+        <![endif]-->
     </head>
 
     <body>
@@ -74,10 +81,11 @@ require_once 'configuration.php';
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </a>
-                        <a class="brand" href="#" name="top">Brand Name</a>
+                        <a class="brand" href="#" name="top">DGHS GIS</a>
                         <div class="nav-collapse collapse">
                             <ul class="nav">
                                 <li><a href="#"><i class="icon-home"></i> Home</a></li>
+                                <!--                                
                                 <li class="divider-vertical"></li>
                                 <li class="active"><a href="#"><i class="icon-file"></i> Pages</a></li>
                                 <li class="divider-vertical"></li>
@@ -86,7 +94,7 @@ require_once 'configuration.php';
                                 <li><a href="#"><i class="icon-signal"></i> Stats</a></li>
                                 <li class="divider-vertical"></li>
                                 <li><a href="#"><i class="icon-lock"></i> Permissions</a></li>
-                                <li class="divider-vertical"></li>
+                                <li class="divider-vertical"></li>-->
                             </ul>
                             <ul class="nav pull-right">
                                 <li><a href="/signup">Sign Up</a></li>
@@ -145,19 +153,30 @@ require_once 'configuration.php';
                                     <p><a href="#" class="btn btn-primary btn-large">Learn more &raquo;</a></p>
                                 </div>-->
                 <div class="row-fluid">
-                    <div id="map" style="width: 600px; height: 400px"></div>
-
+                    <div id="map" style="width: 600px; height: 550px"></div>
+                    <input class="btn btn-info" type="button" onclick="leadRocket();" value="Load Rocket"/>
+                    <input class="btn btn-warning" type="button" onclick="clear_khilgaon();" value="Clear All"/>
+                    <input class="btn btn-primary" type="button" onclick="load_khilgaon();" value="Load Khilgaon"/>
+                    
                     <!-- Leafletjs CDN js link -->
                     <!-- <script src="http://cdn.leafletjs.com/leaflet-0.5/leaflet.js"></script> -->
                     <script src="library/leafletjs/leaflet.js"></script>
                     <script src="library/leaflet.awesome-markers/leaflet.awesome-markers.js"></script>
-
+                    
                     <script>
 
                         // load a map which has the center at the coordinate 23.75092,90.4253
-                        var map = L.map('map').setView([23.75092, 90.4253], 13);
+                        var khilgaonRegion = new L.layerGroup();
+                        var all_UHC = new L.layerGroup();
+                        // var map = L.map('map').setView([23.75092, 90.4253], 13);
+                        var map = L.map('map', {
+                            center : [23.75092, 90.4253],
+                            zoom : 7,
+                            layers: [all_UHC]
+                        });
 
-                        L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+
+                        L.tileLayer('http://{s}.tile.cloudmade.com/bd92fa43571c4092bfc457e9c839d54f/997/256/{z}/{x}/{y}.png', {
                             maxZoom: 18,
                             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
                         }).addTo(map);
@@ -185,30 +204,44 @@ require_once 'configuration.php';
                         });
 
 
-                        L.marker([23.75092, 90.4253]).addTo(map).bindPopup("<b>Khilgaon Thana");
+                        // L.marker([23.75092, 90.4253]).addTo(khilgaonRegion).bindPopup("<b>Khilgaon Thana");
 
-                        L.marker([23.7518, 90.42458], {icon: shoppingCartMarker}).addTo(map).bindPopup("<b>Khilgaon Taltala Market");
+                        // L.marker([23.7518, 90.42458], {icon: shoppingCartMarker}).addTo(khilgaonRegion).bindPopup("<b>Khilgaon Taltala Market");
 
-                        L.marker([23.750635, 90.420871], {icon: homeMarker}).addTo(map).bindPopup("<b>Khilgaon Govt High School");
+                        // L.marker([23.750635, 90.420871], {icon: homeMarker}).addTo(khilgaonRegion).bindPopup("<b>Khilgaon Govt High School");
 
-                        L.marker([23.750277, 90.422378], {icon: homeMarker}).addTo(map).bindPopup("<b>Khilgaon Girls' School And College");
+                        // L.marker([23.750277, 90.422378], {icon: homeMarker}).addTo(khilgaonRegion).bindPopup("<b>Khilgaon Girls' School And College");
 
-                        L.marker([23.735973, 90.425034], {icon: redMarker}).addTo(map).bindPopup("<b>Railway Hospital");
+                        // L.marker([23.735973, 90.425034], {icon: redMarker}).addTo(khilgaonRegion).bindPopup("<b>Railway Hospital");
 
-                        L.marker([23.746363, 90.421128], {icon: hospitalMarker}).addTo(map).bindPopup("<b>Jheel Mosque");
+                        // L.marker([23.746363, 90.421128], {icon: hospitalMarker}).addTo(khilgaonRegion).bindPopup("<b>Jheel Mosque");
 
-                        L.marker([23.746579, 90.412545], {icon: shoppingCartMarker}).addTo(map).bindPopup("<b>Mouchak Market");
+                        // L.marker([23.746579, 90.412545], {icon: shoppingCartMarker}).addTo(khilgaonRegion).bindPopup("<b>Mouchak Market");
 
-                        L.marker([23.770893, 90.414412], {icon: rocketMarker}).addTo(map).bindPopup("<b>Hatir Jheel");
-
+                        <?php
+                            while($location = mysql_fetch_assoc($result)){
+                                $location_name = $location[name];
+                                $latitude = $location[latitude];
+                                $longitude = $location[longitude];
+                                echo "L.marker([$latitude, $longitude], {icon: hospitalMarker}).addTo(all_UHC).bindPopup(\"$location_name\");";
+                            }
+                        ?>
+                        
+                        function leadRocket(){
+                            L.marker([23.770893, 90.414412], {icon: rocketMarker}).addTo(map).bindPopup("<b>Hatir Jheel");
+                        }
+                        function clear_khilgaon(){
+                            khilgaonRegion.clearLayers();
+                        }
+                        function load_khilgaon(){
+                            khilgaonRegion.addTo(map);
+                        }
 
                         var popup = L.popup();
-
                         function onMapClick(e) {
-                            popup
-                                    .setLatLng(e.latlng)
-                                    .setContent("You clicked the map at " + e.latlng.toString())
-                                    .openOn(map);
+                            popup.setLatLng(e.latlng)
+                                 .setContent("You clicked the map at " + e.latlng.toString())
+                                 .openOn(map);
                         }
 
                         map.on('click', onMapClick);
@@ -252,13 +285,13 @@ require_once 'configuration.php';
 
             <hr>
 
-            
+
 
         </div><!--/.fluid-container-->
-        
+
         <footer>
-                <p>&copy; Company 2013</p>
-            </footer>
+            <p>&copy; Company 2013</p>
+        </footer>
 
         <!-- Load javascript
         ================================================== -->
